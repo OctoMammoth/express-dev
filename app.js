4575c42c477fd441cc32720d88f2e8e1217b5114
OctoM <orders@octom.ru>
2021-04-15 15:42:19 +0900

    Inti

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+
+const app = express();
+
+app.use(cors())
+
+app.use(bodyParser.json())
+
+const PORT = process.env.PORT || 8000
+
+app.get('/', (req, res) => {
+    res.send('This page in development')
+})
+
+app.listen(PORT, HOST, () => {
+    console.log('backend running on http://localhost:' + PORT)
+})
